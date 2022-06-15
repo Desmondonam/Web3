a641b4f88a582a189a0b91ee497cad204922a6f0
Desmond Onam <desmondonam@gmail.com>
2022-06-15 22:56:01 +0300

    delete

--- tnbc-js-snippets-main/my-app/fetchIncomingTransactions.js
+++
@@ -1,9 +0,0 @@
-const { axios } = require('axios')
-
-async function fetchIncomingTransactions (bankIP, accountNumber) {
-  const response = await axios.get(`http://${bankIP}/bank_transactions?recipient=${accountNumber}`)
-
-  if (response.status === 200) return response.data
-}
-
-module.exports = fetchIncomingTransactions
